b309da58e8a78ae0d9c487a9c8f08c8e4a7cb36d
gunampallir-cpu <gunampallir-cpu@users.noreply.github.com>
2025-11-27 16:04:16 +0530

    Fix App test: Remove duplicate Router wrapper

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,23 +1,13 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
-import { AuthProvider } from '../contexts/AuthContext';
 
 // Mock components
 jest.mock('../components/Login', () => () => <div>Login Component</div>);
 jest.mock('../components/TemplateGenerator', () => () => <div>Template Generator</div>);
 jest.mock('../components/AdminPanel', () => () => <div>Admin Panel</div>);
 
-const MockApp = () => (
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
-);
-
 describe('App Component', () => {
   test('renders without crashing', () => {
-    render(<MockApp />);
+    render(<App />);
   });
 });
