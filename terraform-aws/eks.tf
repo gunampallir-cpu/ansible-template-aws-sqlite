a91eb0b0c4db3da6ba2e49a599d14e3186f0ab53
gunampallir-cpu <gunampallir-cpu@users.noreply.github.com>
2025-11-27 20:35:37 +0530

    Remove problematic EKS addons (coredns, ebs-csi-driver) from Terraform management

--- terraform-aws/eks.tf
+++ terraform-aws/eks.tf
@@ -96,15 +96,17 @@ resource "aws_eks_addon" "vpc_cni" {
   depends_on = [aws_eks_node_group.main]
 }
 
-resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "coredns"
-
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_eks_node_group.main]
-}
+# CoreDNS addon - commented out due to persistent DEGRADED state
+# Manually managed via kubectl if needed
+# resource "aws_eks_addon" "coredns" {
+#   cluster_name = aws_eks_cluster.main.name
+#   addon_name   = "coredns"
+# 
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+# 
+#   depends_on = [aws_eks_node_group.main]
+# }
 
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name = aws_eks_cluster.main.name
@@ -116,15 +118,17 @@ resource "aws_eks_addon" "kube_proxy" {
   depends_on = [aws_eks_node_group.main]
 }
 
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-ebs-csi-driver"
-
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_eks_node_group.main]
-}
+# EBS CSI Driver addon - commented out due to persistent DEGRADED state
+# Manually managed via Helm if needed
+# resource "aws_eks_addon" "ebs_csi_driver" {
+#   cluster_name = aws_eks_cluster.main.name
+#   addon_name   = "aws-ebs-csi-driver"
+# 
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+# 
+#   depends_on = [aws_eks_node_group.main]
+# }
 
 # CloudWatch Log Group for EKS
 resource "aws_cloudwatch_log_group" "eks" {
